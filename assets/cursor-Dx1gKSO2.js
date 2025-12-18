7f697a10317840aa35cdecae75d4b593dafa2f46
Sh11thead <Sh11thead@users.noreply.github.com>
2025-12-18 02:36:36 +0000

    deploy: b44a7ebf18c6cdacd963a03d9f9c5c99ed0d44c6

---
+++ assets/cursor-Dx1gKSO2.js
@@ -0,0 +1,3 @@
+import{b as o}from"./core-XRgChQy8.js";import"./index-CKqnnY0d.js";import"./events-s55Gzao4.js";import"./index.es-COArnetJ.js";import"./index-nibyPLVP.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
+  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
+</svg>`;export{l as cursorSvg};
