6b0828569e71ab8d41941bb5b8ca68b9228d401c
Sh11thead <Sh11thead@users.noreply.github.com>
2026-04-30 08:08:46 +0000

    deploy: f3ca26410042f55309ded5b61f7d68b077e5d76d

---
+++ assets/chevron-bottom-7hPF_qLR.js
@@ -0,0 +1,8 @@
+import{b as o}from"./core-BzBeMs5T.js";import"./index-cp4NIfFm.js";import"./events-C6wYdj3D.js";import"./index.es-CaQko9uj.js";import"./index-nibyPLVP.js";const p=o`<svg fill="none" viewBox="0 0 16 16">
+  <path
+    fill="currentColor"
+    fill-rule="evenodd"
+    d="M1.46 4.96a1 1 0 0 1 1.41 0L8 10.09l5.13-5.13a1 1 0 1 1 1.41 1.41l-5.83 5.84a1 1 0 0 1-1.42 0L1.46 6.37a1 1 0 0 1 0-1.41Z"
+    clip-rule="evenodd"
+  />
+</svg>`;export{p as chevronBottomSvg};
